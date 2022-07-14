22489a9af2a6fd0a6dd3dd82b130112a7ee51662
Guilherme Fernandes <guilheermeff@outlook.com>
2022-07-14 01:06:43 -0400

    add classes js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 // classe que vai fazer a lógica dos dados
+export class Favorites {
+  constructor(root){
+    this.root = document.querySelector(root)
+  }
+}
 // classe que vai criar a vizualização e eventos html
+export class FavoritesView extends Favorites {
+  constructor(root){
+    super(root)
+
+    console.log(this.root)
+  }
+}
