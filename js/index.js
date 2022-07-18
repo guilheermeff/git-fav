29c111564fe253505307091df7f49f5406249d89
Guilherme Fernandes <guilheermeff@outlook.com>
2022-07-18 11:32:30 -0400

    update js

--- js/index.js
+++ js/index.js
@@ -3,12 +3,15 @@ export class Favorites {
   constructor(root){
     this.root = document.querySelector(root)
   }
+
+  // funções: carregar, salvar, deletar usuário, adicionar usuário
 }
 // classe que vai criar a vizualização e eventos html
 export class FavoritesView extends Favorites {
   constructor(root){
     super(root)
-
+    
     console.log(this.root)
   }
 }
+
